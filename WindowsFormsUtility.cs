c27c851915cb8a4837bb1b837043693d422cf785
Shmiel Minz <s0583223395@gmail.com>
2023-03-20 20:32:53 +0000

    Session 23 done

--- WindowsFormsUtility.cs
+++ WindowsFormsUtility.cs
@@ -17,7 +17,7 @@ namespace CPUWindowsFormFramework
             lst.DataBindings.Add("SelectedValue", childtable, lst.ValueMember, false, DataSourceUpdateMode.OnPropertyChanged);
         }
 
-        public static void SetControlBinding(Control ctrl, DataTable dt)
+        public static void SetControlBinding(Control ctrl, BindingSource bindsource)
         {
             string propertyname = "";
             string controlname = ctrl.Name.ToLower();
@@ -37,7 +37,7 @@ namespace CPUWindowsFormFramework
 
             if (propertyname != "" && columnname != "")
             {
-                ctrl.DataBindings.Add(propertyname, dt, columnname, true, DataSourceUpdateMode.OnPropertyChanged);
+                ctrl.DataBindings.Add(propertyname, bindsource, columnname, true, DataSourceUpdateMode.OnPropertyChanged);
             }
 
         }
